15e1cf4fa8696d805c06b4a122791b3b0a46ab2e
henrychucvb01 <henrychucvb01@yahoo.com>
2026-09-02 20:01:15 -0700

    Add dedicated About Huy route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import './hero-reference.css'
+import AboutPage from './components/AboutPage'
 import Endorsements from './components/Endorsements'
 import ExperienceStrip from './components/ExperienceStrip'
 import Footer from './components/Footer'
@@ -9,19 +10,27 @@ import Hero from './components/Hero'
 import ThreeFs from './components/ThreeFs'
 import WhyHuy from './components/WhyHuy'
 
+function HomePage() {
+  return (
+    <main>
+      <Hero />
+      <ThreeFs />
+      <WhyHuy />
+      <ExperienceStrip />
+      <Endorsements />
+      <span className="anchor-target" id="news" aria-hidden="true" />
+      <GetInvolved />
+    </main>
+  )
+}
+
 function App() {
+  const isAboutPage = window.location.pathname === '/about' || window.location.pathname === '/about/'
+
   return (
     <>
       <Header />
-      <main>
-        <Hero />
-        <ThreeFs />
-        <WhyHuy />
-        <ExperienceStrip />
-        <Endorsements />
-        <span className="anchor-target" id="news" aria-hidden="true" />
-        <GetInvolved />
-      </main>
+      {isAboutPage ? <AboutPage /> : <HomePage />}
       <Footer />
     </>
   )
